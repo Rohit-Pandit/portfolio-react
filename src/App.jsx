d8f973f3d450ad49fbf8b02f055e70a34e3aef14
Rohit-Pandit <rohitpandit.190425@gmail.com>
2025-08-04 12:34:02 +0530

    about me section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import Home from './Components/Home/Home.jsx';
+import About from './Components/About/About.jsx';
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Home/>
+      <About/>
     </div>
   )
 }
---
+++ src/Components/About/About.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './About.css';
+import profile from './../../assets/profile.jpg'
+
+const About = () => {
+  return (
+    <div className='about'>
+        <div className="about-title">
+            <h1>About me</h1>
+            {/* <img src="" alt="About me img" /> */}
+        </div>
+        <div className="about-section">
+            <div className="about-left">
+                <img src={profile} alt="" />
+            </div>
+            <div className="about-right">
+                <div className="about-para">
+                    <p>Some detail for portfolio user</p>
+                    <p>My passion for web development </p>
+                </div>
+                <div className="about-skills">
+                    <div className="about-skill"><p>HTML & CSS</p><hr style={{width:"50%"}}/></div>
+                    <div className="about-skill"><p>React Js</p><hr style={{width:"70%"}}/></div>
+                    <div className="about-skill"><p>JavaScript</p><hr style={{width:"60%"}}/></div>
+                    
+                </div>
+            </div>
+        </div>
+        <div className="about-achievements">
+            <div className="about-achievement">
+                <h1>1+</h1>
+                <p>YEARS OF EXPERIENCE</p>
+            </div>
+            <hr />
+            <div className="about-achievement">
+                <h1>5+</h1>
+                <p>PROJECTS COMPLETED</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
