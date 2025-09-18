0028105aa29e0bc4ce18fe30d06e533f6726f2c2
Rohit-Pandit <rohitpandit.190425@gmail.com>
2025-09-18 17:16:34 +0530

    refacoted about component

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,44 +1,66 @@
 import React from 'react';
 import './About.css';
-import profile from './../../assets/profile.jpg'
+import profile from './../../assets/profile.jpg';
 
 const About = () => {
   return (
-    <div className='about'>
-        <div className="about-title">
-            <h1>About me</h1>
-            {/* <img src="" alt="About me img" /> */}
-        </div>
-        <div className="about-section">
-            <div className="about-left">
-                <img src={profile} alt="" />
-            </div>
-            <div className="about-right">
-                <div className="about-para">
-                    <p>Some detail for portfolio user</p>
-                    <p>My passion for web development </p>
-                </div>
-                <div className="about-skills">
-                    <div className="about-skill"><p>HTML & CSS</p><hr style={{width:"100%"}}/></div>
-                    <div className="about-skill"><p>React Js</p><hr style={{width:"70%"}}/></div>
-                    <div className="about-skill"><p>JavaScript</p><hr style={{width:"60%"}}/></div>
-                    
-                </div>
-            </div>
-        </div>
-        <div className="about-achievements">
-            <div className="about-achievement">
-                <h1>1+</h1>
-                <p>YEARS OF EXPERIENCE</p>
-            </div>
-            <hr />
-            <div className="about-achievement">
-                <h1>5+</h1>
-                <p>PROJECTS COMPLETED</p>
-            </div>
-        </div>
-    </div>
-  )
-}
+    <section className="about" id="about">
+      <div className="about-title">
+        <h1>About Me</h1>
+      </div>
 
-export default About
+      <div className="about-section">
+        {/* Left: Profile image */}
+        <div className="about-left">
+          <img src={profile} alt="About Rohit Pandit" />
+        </div>
+
+        {/* Right: Content */}
+        <div className="about-right">
+          <div className="about-para">
+            <p>
+              I’m a <span>Software Developer</span> passionate about building modern web
+              applications. I enjoy solving problems, creating intuitive UIs, and writing
+              clean, scalable code.
+            </p>
+            <p>
+              Over the past <span>1.4 years</span>, I’ve gained hands-on experience working
+              with <strong>React.js, JavaScript, and REST APIs</strong> while also improving
+              my fundamentals in <strong>HTML & CSS</strong>.
+            </p>
+          </div>
+
+          {/* Skills */}
+          <div className="about-skills">
+            <div className="about-skill">
+              <p>HTML & CSS</p>
+              <div className="skill-bar"><div className="skill-level" style={{width:"95%"}}></div></div>
+            </div>
+            <div className="about-skill">
+              <p>React.js</p>
+              <div className="skill-bar"><div className="skill-level" style={{width:"80%"}}></div></div>
+            </div>
+            <div className="about-skill">
+              <p>JavaScript</p>
+              <div className="skill-bar"><div className="skill-level" style={{width:"75%"}}></div></div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Achievements */}
+      <div className="about-achievements">
+        <div className="about-achievement">
+          <h1>1+</h1>
+          <p>Years of Experience</p>
+        </div>
+        <div className="about-achievement">
+          <h1>5+</h1>
+          <p>Projects Completed</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
